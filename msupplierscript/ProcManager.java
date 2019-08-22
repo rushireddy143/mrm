f4c27c52f115190a66cda6d7fd06455e037c19c3
lrushikeswarreddy <52237938+lrushikeswarreddy@users.noreply.github.com>
2019-08-22 19:32:15 +0530

    Update ProcManager.java

--- msupplierscript/ProcManager.java
+++ msupplierscript/ProcManager.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ProcManager {
-
+//rushi
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
